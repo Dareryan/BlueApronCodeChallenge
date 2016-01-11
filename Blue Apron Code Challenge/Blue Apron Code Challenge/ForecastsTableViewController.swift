61e25126bf003dadcb032dfffdf62bb81087c991
Dare Ryan <Dareryan@me.com>
2016-01-11 15:23:08 -0500

    Hookup Table View Controller to ForecastDataSource

--- Blue Apron Code Challenge/Blue Apron Code Challenge/ForecastsTableViewController.swift
+++ Blue Apron Code Challenge/Blue Apron Code Challenge/ForecastsTableViewController.swift
@@ -8,19 +8,70 @@
 
 import UIKit
 
-class ForecastsTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-
-    // MARK: - Table view data source
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 0
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
+class ForecastsTableViewController: UITableViewController, ForecastDataSourceDelegate {
+   
+   let dataSource = ForecastDataSource()
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      setupTableView()
+      setupDataSource()
+   }
+   
+   func setupTableView() {
+      tableView.registerNib(UINib(nibName: "ForecastTableViewCell", bundle: nil), forCellReuseIdentifier: ForecastTableViewCell.reuseIdentifier())
+   }
+   
+   func setupDataSource () {
+      dataSource.delegate = self
+      dataSource.reload()
+   }
+   
+   // MARK: - Table view data source
+   
+   override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+      return dataSource.numberOfSections()
+   }
+   
+   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+      return dataSource.numberOfRowsInSection(section)
+   }
+   
+   func forecastDataSourceDidLoad(dataSource: ForecastDataSource) {
+      dispatch_async(dispatch_get_main_queue()) { () -> Void in
+         self.tableView.reloadData()
+      }
+   }
+   
+   override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+      return dataSource.titleForSection(section)
+   }
+   
+   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+      let cell = tableView.dequeueReusableCellWithIdentifier(ForecastTableViewCell.reuseIdentifier(), forIndexPath: indexPath) as! ForecastTableViewCell
+      
+      let forecast = dataSource.forecastForIndexPath(indexPath)
+      let forecastTime = dataSource.generateForecastTimeStamp(forecast.forecastTime)
+      let forecastCity = dataSource.cityName
+      let forecastDescription = forecast.weatherDescription
+      let forecastTemp = String(format: "%.1f˚F", forecast.currentTemp)
+      let forecastMin = String(format: "%.1f˚F", forecast.minimumTemp)
+      let forecastMax = String(format: "%.1f˚F", forecast.maximumTemp)
+      var forecastIcon: UIImage
+      if let icon = UIImage(named: forecast.weatherIconID) {
+         forecastIcon = icon
+      }else{
+         //Default to sun image in case of error
+         forecastIcon = UIImage(named: "01d")!
+      }
+      
+      cell.configureWithForecastData(forecastTime, city: forecastCity, description: forecastDescription, image: forecastIcon, temp: forecastTemp, high: forecastMax, low: forecastMin)
+      
+      return cell
+   }
+   
+   override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+      return ForecastTableViewCell.cellHeight()
+   }
+   
 }
