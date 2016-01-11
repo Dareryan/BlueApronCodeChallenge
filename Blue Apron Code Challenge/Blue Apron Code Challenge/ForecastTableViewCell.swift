3b46d83dcce37e2ef4c342085e9524a81c52aa60
Dare Ryan <Dareryan@me.com>
2016-01-11 15:04:52 -0500

    Add forecast table view cell xib and class with configuration method

---
+++ Blue Apron Code Challenge/Blue Apron Code Challenge/ForecastTableViewCell.swift
@@ -0,0 +1,41 @@
+//
+//  ForecastTableViewCell.swift
+//  Blue Apron Code Challenge
+//
+//  Created by Dare Ryan on 1/11/16.
+//  Copyright © 2016 co.cordproject. All rights reserved.
+//
+
+import UIKit
+
+class ForecastTableViewCell: UITableViewCell {
+   
+   //MARK: instance variables
+   
+   @IBOutlet weak var timeLabel: UILabel!
+   @IBOutlet weak var cityLabel: UILabel!
+   @IBOutlet weak var descriptionLabel: UILabel!
+   @IBOutlet weak var weatherImageView: UIImageView!
+   @IBOutlet weak var currentTempLabel: UILabel!
+   @IBOutlet weak var highTempLabel: UILabel!
+   @IBOutlet weak var lowTempLabel: UILabel!
+   
+   func configureWithForecastData(time: String, city: String, description: String, image: UIImage, temp: String, high: String, low: String) {
+      timeLabel.text = time
+      cityLabel.text = city
+      descriptionLabel.text = description
+      weatherImageView.image = image
+      currentTempLabel.text = temp
+      highTempLabel.text = high
+      lowTempLabel.text = low
+   }
+   
+   class func cellHeight() -> CGFloat {
+      return 90.0
+   }
+   
+   class func reuseIdentifier() -> String {
+      return "ForecastTableViewCell"
+   }
+}
+
