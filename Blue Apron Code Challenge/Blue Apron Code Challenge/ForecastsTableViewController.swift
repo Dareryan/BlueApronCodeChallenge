defe174d5ed59b5e9b24ac5c1315798ef60add9f
Dare Ryan <Dareryan@me.com>
2016-01-11 14:10:15 -0500

    Add ForecastsTableViewController class and hookup to TableViewController in main.storyboard

---
+++ Blue Apron Code Challenge/Blue Apron Code Challenge/ForecastsTableViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ForecastsTableViewController.swift
+//  Blue Apron Code Challenge
+//
+//  Created by Dare Ryan on 1/11/16.
+//  Copyright © 2016 co.cordproject. All rights reserved.
+//
+
+import UIKit
+
+class ForecastsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+}
